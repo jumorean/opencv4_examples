d21608f73a58d09d28d68489df77572721d0311b
jumorean <jumorean@outlook.com>
2020-10-29 11:34:26 +0800

    最简单范例程序

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main() {
+
+    cv::Mat a;
+
+    a = cv::imread("/home/cda/Pictures/timg.jpeg", cv::IMREAD_UNCHANGED);
+
+    // std::cout << a.size();
+    cv::imshow("img", a);
+    cv::waitKey(0);
+    return 0;
+}
